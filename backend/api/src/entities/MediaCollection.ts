85124db05734950e4a47225c9cb8b0e53c92313e
Igor Zimmermann <igorzimmermann01@gmail.com>
2022-03-14 12:25:14 +0100

    [ADD] Join tables for Media relationships
    [Add]
    - MediaCollection entity for Media Collection many-to-many relationship
    - MediaStudio entity for Media Studio many-to-many relationship

---
+++ backend/api/src/entities/MediaCollection.ts
@@ -0,0 +1,47 @@
+import { Field, ID, InputType, ObjectType } from 'type-graphql'
+import {
+	BaseEntity,
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from 'typeorm'
+
+@ObjectType()
+@Entity()
+export class MediaCollection extends BaseEntity {
+	@Field(() => ID)
+	@PrimaryGeneratedColumn('uuid')
+	id: string
+
+	@CreateDateColumn()
+	createdAt: Date
+
+	@UpdateDateColumn()
+	updatedAt: Date
+
+	@Field(() => ID)
+	@Column('text')
+	collectionId: string
+
+	@Field(() => ID)
+	@Column('text')
+	mediaId: string
+
+	@Field(() => String, { nullable: true })
+	@Column('text', { nullable: true })
+	comment?: string
+}
+
+@InputType()
+export class MediaCollectionInput {
+	@Field(() => ID)
+	collectionId: string
+
+	@Field(() => ID)
+	mediaId: string
+
+	@Field(() => String, { nullable: true })
+	comment?: string
+}
---
+++ backend/api/src/entities/MediaStudio.ts
@@ -0,0 +1,40 @@
+import { Field, ID, InputType, ObjectType } from 'type-graphql'
+import {
+	BaseEntity,
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from 'typeorm'
+
+@ObjectType()
+@Entity()
+export class MediaStudio extends BaseEntity {
+	@Field(() => ID)
+	@PrimaryGeneratedColumn('uuid')
+	id: string
+
+	@CreateDateColumn()
+	createdAt: Date
+
+	@UpdateDateColumn()
+	updatedAt: Date
+
+	@Field(() => ID)
+	@Column('text')
+	studioId: string
+
+	@Field(() => ID)
+	@Column('text')
+	mediaId: string
+}
+
+@InputType()
+export class MediaStudioInput {
+	@Field(() => ID)
+	studioId: string
+
+	@Field(() => ID)
+	mediaId: string
+}
