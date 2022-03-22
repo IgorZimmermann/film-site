e37d6443c6896ab0494368e89445e17dcc735ce9
Igor Zimmermann <igorzimmermann01@gmail.com>
2022-03-22 18:36:00 +0100

    [FIX] Fix #1 for file server

--- backend/files/src/index.ts
+++ backend/files/src/index.ts
@@ -1,12 +1,11 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import { join } from 'path'
 
 dotenv.config()
 
 const app = express()
 
-app.use('/static', express.static(join(__dirname, process.env.CONTENT_VOLUME!)))
+app.use('/static', express.static(process.env.CONTENT_VOLUME!))
 
 app.get('/ho', (_, res) => {
 	res.send('ho')
